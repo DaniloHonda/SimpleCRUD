cbe746b951447de5104eb67bf1e944baa864ab33
Danilo Honda <hondadanilo6@gmail.com>
2025-01-25 14:18:00 -0300

    add login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,30 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+    
+        if (Auth::attempt([
+            'name' => $incomingFields['loginname'], 
+            'password' => $incomingFields['loginpassword']
+        ])) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+    
+        return back()->withErrors([
+            'loginname' => 'Invalid credentials'
+        ]);
+    }
+
     public function logout() {
         Auth::logout();
         return redirect('/');
     }
+
     public function register(Request $request) {
         $incomingFields = $request->validate([
             'name' => ['required', Rule::unique('users', 'name')],
